561e0836589adb71a776badb5d3ac36ebfa102b0
tanglei <gems8@tanglei.name>
2015-04-18 22:54:45 +0800

    gjk example 2d init ok

--- figures/GJKExample2D.py
+++ figures/GJKExample2D.py
@@ -4,7 +4,25 @@ diff = []
 for q in quad:
     for t in tri:
         diff += [(q[0]-t[0], q[1]-t[1])]
-print set(diff)
+#print set(diff)
+
+dot = -1e10
+d = (-21.25, -8.75)
+maxDot = ()
+for q in quad:
+    t = q[0]*d[0]+q[1]*d[1]
+    if t>dot:
+        maxDot = q
+print maxDot
+d = (-d[0], -d[1])
+dot = -1e10
+for q in tri:
+    t = q[0]*d[0]+q[1]*d[1]
+    if t>dot:
+        maxDot = q
+print maxDot
+
+exit()
 
 quad = [(-4, 2), (-2.5, 2), (-2.5, 3.5), (-4, 3.5)]
 tri = [(-3, 1), (-1,3), (-1, 1)]
